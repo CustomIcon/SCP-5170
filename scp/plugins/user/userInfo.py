9dee2d0ae64f64a06111098ac8ce4ca9e02de515
ポキ <aman_a@aol.com>
2021-08-20 03:54:43 -0700

    hamcker wanted this

--- scp/plugins/user/userInfo.py
+++ scp/plugins/user/userInfo.py
@@ -1,5 +1,7 @@
+from scp.utils.strUtils import permParse
 from scp import user, bot
 from scp.utils.selfInfo import info
+from scp.utils.strUtils import name_check, permParse
 from pyrogram import errors
 
 
@@ -57,14 +59,23 @@ async def _(_, query: bot.types.InlineQuery):
     if isinstance(u, user.types.Chat):
         text = user.md.Section(
             'ChatInfo:',
-            user.md.KeyValueItem(key='chat_id', value=u.id),
-            user.md.KeyValueItem(key='type', value=u.type),
-            user.md.KeyValueItem(key='title', value=u.title),
-            user.md.KeyValueItem(key='invite_link', value=u.invite_link),
-            user.md.KeyValueItem(key='members_count', value=u.members_count),
-            user.md.KeyValueItem(key='dc_id', value=u.dc_id),
-            user.md.KeyValueItem(key='username', value=u.username)
-        )
+            user.md.SubSection(
+                user.md.KeyValueItem('title', u.title),
+            user.md.KeyValueItem(
+                user.md.Bold('chat_id'), user.md.Code(u.id)),
+            user.md.KeyValueItem(
+                user.md.Bold('type'), user.md.Code(u.type)),
+            user.md.KeyValueItem(
+                user.md.Bold('title'), user.md.Code(u.title)),
+            user.md.KeyValueItem(
+                user.md.Bold('invite_link'), user.md.Code(u.invite_link)),
+            user.md.KeyValueItem(
+                user.md.Bold('members_count'), user.md.Code(u.members_count)),
+            user.md.KeyValueItem(
+                user.md.Bold('dc_id'), user.md.Code(u.dc_id)),
+            user.md.KeyValueItem(
+                user.md.Bold('username'), user.md.Code(name_check(u.username)))
+        ))
         keyboard = user.types.InlineKeyboardMarkup(
             [
                 [
@@ -76,13 +87,13 @@ async def _(_, query: bot.types.InlineQuery):
     else:
         text = user.md.Section(
             'UserInfo:',
-            user.md.KeyValueItem(key='user_id', value=u.id),
-            user.md.KeyValueItem(key='is_contact', value=u.is_contact),
-            user.md.KeyValueItem(key='first_name', value=u.first_name),
-            user.md.KeyValueItem(key='last_name', value=u.last_name),
-            user.md.KeyValueItem(key='username', value=u.username),
-            user.md.KeyValueItem(key='dc_id', value=u.dc_id)
-        )
+            user.md.SubSection(
+                user.md.KeyValueItem(key='name', value=u.first_name + ' ' + (u.last_name or '')),
+            user.md.KeyValueItem(user.md.Bold('user_id'), user.md.Code(u.id)),
+            user.md.KeyValueItem(user.md.Bold('is_contact'), user.md.Code(u.is_contact)),
+            user.md.KeyValueItem(user.md.Bold('username'), user.md.Code(name_check(u.username))),
+            user.md.KeyValueItem(user.md.Bold('dc_id'), user.md.Code(u.dc_id))
+        ))
         keyboard = user.types.InlineKeyboardMarkup(
             [[user.types.InlineKeyboardButton('UserLink', url=f'tg://user?id={u.id}'), user.types.InlineKeyboardButton('Description', callback_data=f'cdesc_{u.id}')]]
         )
@@ -100,33 +111,10 @@ async def _(_, query: bot.types.InlineQuery):
     )
 
 
-def bool_check(var: bool):
-    if var:
-        return "✅"
-    else:
-        return "❌"
-
-
-def _text(perms):
-    text = ''
-    text += 'Message: ' + bool_check(perms.can_send_messages) + '\n'
-    text += 'Media: ' + bool_check(perms.can_send_media_messages) + '\n'
-    text += 'Sticker: ' + bool_check(perms.can_send_stickers) + '\n'
-    text += 'GIF: ' + bool_check(perms.can_send_animations) + '\n'
-    text += 'Game: ' + bool_check(perms.can_send_games) + '\n'
-    text += 'Inline: ' + bool_check(perms.can_use_inline_bots) + '\n'
-    text += 'Web: ' + bool_check(perms.can_add_web_page_previews) + '\n'
-    text += 'Poll: ' + bool_check(perms.can_send_polls) + '\n'
-    text += 'Info: ' + bool_check(perms.can_change_info) + '\n'
-    text += 'invite: ' + bool_check(perms.can_invite_users) + '\n'
-    text += 'Pin: ' + bool_check(perms.can_pin_messages) + '\n'
-    return text
-
-
 @bot.on_callback_query(bot.sudo & bot.filters.regex('^cperm_'))
 async def _(_, query:user.types.CallbackQuery):
     await query.answer(
-        _text(
+        permParse(
             (await user.get_chat(int(query.data.split("_")[1]))).permissions
         ), show_alert=True)
 
---
+++ scp/utils/strUtils.py
@@ -0,0 +1,30 @@
+from pyrogram.types import ChatPermissions
+
+
+def name_check(username: str = None):
+    if username:
+        return '@'+username
+    return None
+
+
+def bool_check(var: bool):
+    if var:
+        return "✅"
+    else:
+        return "❌"
+
+
+def _text(perms: ChatPermissions):
+    text = ''
+    text += 'Message: ' + bool_check(perms.can_send_messages) + '\n'
+    text += 'Media: ' + bool_check(perms.can_send_media_messages) + '\n'
+    text += 'Sticker: ' + bool_check(perms.can_send_stickers) + '\n'
+    text += 'GIF: ' + bool_check(perms.can_send_animations) + '\n'
+    text += 'Game: ' + bool_check(perms.can_send_games) + '\n'
+    text += 'Inline: ' + bool_check(perms.can_use_inline_bots) + '\n'
+    text += 'Web: ' + bool_check(perms.can_add_web_page_previews) + '\n'
+    text += 'Poll: ' + bool_check(perms.can_send_polls) + '\n'
+    text += 'Info: ' + bool_check(perms.can_change_info) + '\n'
+    text += 'invite: ' + bool_check(perms.can_invite_users) + '\n'
+    text += 'Pin: ' + bool_check(perms.can_pin_messages) + '\n'
+    return text
