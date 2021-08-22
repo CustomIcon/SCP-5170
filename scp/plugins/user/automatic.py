23a97a1cf36f4a8082abc1232a9058e49971693c
ポキ <aman_a@aol.com>
2021-08-22 13:28:34 -0700

    a better spam detector
    Co-Authored-By: Davide Galilei <43778739+DavideGalilei@users.noreply.github.com>

--- scp/plugins/user/automatic.py
+++ scp/plugins/user/automatic.py
@@ -1,7 +1,10 @@
 from scp import user, bot
-from scp.utils.spamCheck import is_flood
+from scp.utils.spamCheck import is_flood, cleaner
+import asyncio
+
 
 @user.on_message(
+    ~user.filters.bot &
     ~user.sudo &
     ~user.filters.chat(
         [int(i) for i in user._config.get('scp-5170', 'IgnoreGroups').split()]
@@ -13,10 +16,10 @@ async def _(_, message: user.types.Message):
     a function to log Spam Detection
     """
     if message.from_user:
-        uid = message.from_user.id
+        uid = message.from_user
     else:
-        uid = message.sender_chat.id
-    if is_flood(uid):
+        uid = message.sender_chat
+    if await is_flood(uid):
         return await bot.send_message(
             user.log_channel,
             user.md.KanTeXDocument(
@@ -26,7 +29,7 @@ async def _(_, message: user.types.Message):
                             user.md.Bold('chat_id'), user.md.Code(message.chat.id)
                         ),
                         user.md.KeyValueItem(
-                            user.md.Bold('user_id'), user.md.Code(uid)
+                            user.md.Bold('user_id'), user.md.Code(uid.id)
                         ),
                         user.md.KeyValueItem(
                             user.md.Bold('message_id'), user.md.Code(message.message_id))
@@ -73,3 +76,6 @@ async def _(_, message: user.types.Message):
                 url=message.reply_to_message.link if message.reply_to_message else message.link)]]
         )
     )
+
+
+asyncio.create_task(cleaner())
--- scp/utils/spamCheck.py
+++ scp/utils/spamCheck.py
@@ -2,14 +2,38 @@
 from time import time
 from collections import defaultdict
 from typing import Union
+from pyrogram import types
+import asyncio
+
 
 USERS = defaultdict(list)
 MESSAGES = 4
 SECONDS = 6
 
 
-def is_flood(uid: int) -> Union[bool, None]:
+async def is_flood(
+    user: types.User,
+    messages: int = MESSAGES,
+    seconds: int = SECONDS,
+    users: defaultdict = USERS,
+) -> Union[bool, None]:
     """Checks if a user is flooding"""
-    USERS[uid].append(time())
-    if len(list(filter(lambda x: time() - int(x) < SECONDS, USERS[uid]))) > MESSAGES:
+    users[user.id].append(time())
+    check = list(filter(lambda x: time() - int(x) < seconds, users[user.id]))
+    if len(check) > messages:
+        users[user.id] = check
         return True
+
+
+async def cleaner(
+    users: defaultdict = USERS,
+    sleep: float = 30,
+    seconds: int = SECONDS,
+):
+    while not await asyncio.sleep(sleep):
+        for user, _ in users.copy().items():
+            check = list(
+                filter(lambda x: time() - int(x) < seconds, users[user])
+            )
+            if not check:
+                del users[user]
