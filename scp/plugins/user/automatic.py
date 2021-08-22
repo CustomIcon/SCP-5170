53cd6c4b7649010be5cd8287ceb3823f7711ee7f
ポキ <aman_a@aol.com>
2021-08-22 02:43:44 -0700

    Update automatic.py

--- scp/plugins/user/automatic.py
+++ scp/plugins/user/automatic.py
@@ -37,3 +37,39 @@ async def _(_, message: user.types.Message):
                 [[user.types.InlineKeyboardButton('message.link', url=message.link)]]
             )
         )
+
+
+@user.on_message(
+    user.filters.group
+    & (user.filters.regex(r'@(a|A)dmins') | user.command('report'))
+)
+async def _(_, message: user.types.Message):
+    """
+    [WIP] a function to log admin calls
+    """
+    if message.from_user:
+        uid = message.from_user.id
+    else:
+        uid = message.sender_chat.id
+    return await bot.send_message(
+        user.log_channel,
+        user.md.KanTeXDocument(
+            user.md.Section('#Report',
+                user.md.SubSection(message.chat.title,
+                    user.md.KeyValueItem(
+                        user.md.Bold('chat_id'), user.md.Code(message.chat.id)
+                    ),
+                    user.md.KeyValueItem(
+                        user.md.Bold('user_id'), user.md.Code(uid)
+                    ),
+                    user.md.KeyValueItem(
+                        user.md.Bold('message_id'), user.md.Code(message.message_id))
+                    )
+                )
+        ),
+        reply_markup=user.types.InlineKeyboardMarkup(
+            [[user.types.InlineKeyboardButton(
+                'message.link',
+                url=message.reply_to_message.link if message.reply_to_message else message.link)]]
+        )
+    )
