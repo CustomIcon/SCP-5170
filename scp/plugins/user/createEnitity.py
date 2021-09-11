d9606d62f9f82b29af8f478fcf602e76305d7480
ポキ <aman_a@aol.com>
2021-09-11 13:07:04 -0700

    quick fix for bot token generator

--- scp/plugins/user/createEnitity.py
+++ scp/plugins/user/createEnitity.py
@@ -40,7 +40,8 @@ async def _(_, message: user.types.Message):
                 user.md.Section(
                     'Error',
                     user.md.Italic(
-                        f'{arg[0].lower()} is not in [\'group\', \'channel\', \'bot\']',
+                        f'{arg[0].lower()}'
+                        'is not in [\'group\', \'channel\', \'bot\']',
                     ),
                 ),
             ),
@@ -55,30 +56,37 @@ async def _(_, message: user.types.Message):
         await asyncio.sleep(0.5)
         await user.send_message('Botfather', arg[1])
         await asyncio.sleep(0.5)
-        ans = await user.ask('Botfather', arg[1].replace(' ', '_')+'_bot')
+        botName = arg[1].replace(' ', '_') + '_bot'
+        ans = await user.ask('Botfather', botName)
         if not ans.text.startswith('Done!'):
             return await message.reply(
                 user.md.KanTeXDocument(
                     user.md.Section(
                         'Error',
                         user.md.Italic(
-                            f'@{arg[1]}_bot is taken',
+                            f'@{botName} is taken',
                         ),
                     ),
                 ),
                 quote=True,
             )
-        _, token = check(ans.text)
-        return await message.reply(
+        validate, token = check(ans.text)
+        if validate:
+            return await message.reply(
                 user.md.KanTeXDocument(
                     user.md.Section(
                         'Generated Token',
-                        user.md.KeyValueItem(user.md.Code('@'+arg[1]+'_bot'), user.md.Code(token))
+                        user.md.KeyValueItem(
+                            user.md.Code(
+                                botName,
+                            ),
+                            user.md.Code(token),
+                        ),
                     ),
                 ),
                 quote=True,
             )
-        
+
     link = await user.export_chat_invite_link(chat.id)
     await message.reply(
         user.md.KanTeXDocument(
@@ -104,16 +112,22 @@ async def _(_, message: user.types.Message):
 
 
 def validate(token: str) -> bool:
-    t = token.partition(":")
+    t = token.partition(':')
     try:
-        decode = base64.urlsafe_b64decode(f"{t[-1]}====")
+        decode = base64.urlsafe_b64decode(f'{t[-1]}====')
     except binascii.Error:
         return False
-    return all((t[0].isdecimal(), decode[:2].startswith(b"\x00\x01"), len(decode)))
+    return all(
+        (
+            t[0].isdecimal(),
+            decode[:2].startswith(b'\x00\x01'),
+            len(decode),
+        ),
+    )
 
 
 def check(token: str) -> bool:
-    token = re.findall(r"[0-9]{10}:[a-zA-Z0-9_-]{35}", token)
+    token = re.findall(r'[0-9]{10}:[a-zA-Z0-9_-]{35}', token)
     if len(token) == 0:
         return False, False
     else:
